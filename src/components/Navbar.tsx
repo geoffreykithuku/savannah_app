0fb139cdb6581a207c8ae23a7e3d921f8f26682d
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-10-21 13:47:40 +0300

    Styled Navbar to be responsive and updated the navlinks to include signin and signup.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { Menu, X } from 'lucide-react';
 import { NavLink } from 'react-router-dom';
 
-
 const Navbar: React.FC = () => {
   // State for controlling the menu toggle (open/close)
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -14,18 +13,19 @@ const Navbar: React.FC = () => {
 
   return (
     <div className="bg-[#351D5B] text-white px-5 sm:px-10 md:px-20 flex w-full items-center justify-between py-6 relative">
-      <h2>SIL APP</h2>
+      <h2 className="flex-shrink-0 z-10">SIL APP</h2>
 
       <div className="w-full mx-auto">
         <ul
           className={`${
             isOpen
-              ? 'flex flex-col justify-center items-center absolute w-full mx-auto gap-3 bg-[#351D5B] py-5 top-20 left-0 rounded'
+              ? 'flex flex-col justify-center items-center absolute w-full mx-auto gap-3 bg-[#351D5B] py-20 z-0 top-0 left-0 rounded'
               : 'hidden md:flex gap-4 lg:gap-10 md:justify-end items-center ml-auto w-full'
           }`}
         >
           <li>
             <NavLink
+              className="hover:text-[#9FC315]"
               style={({ isActive }: { isActive: boolean }) => {
                 return isActive ? { color: '#9FC315' } : {};
               }}
@@ -36,27 +36,29 @@ const Navbar: React.FC = () => {
           </li>
           <li>
             <NavLink
+              className="hover:text-[#9FC315]"
               style={({ isActive }: { isActive: boolean }) => {
                 return isActive ? { color: '#9FC315' } : {};
               }}
-              to="/unsplash"
+              to="/login"
             >
-              
+              Login
             </NavLink>
           </li>
           <li>
             <NavLink
+              className="hover:text-[#9FC315]"
               style={({ isActive }: { isActive: boolean }) => {
                 return isActive ? { color: '#9FC315' } : {};
               }}
-              to="/rick-morty"
+              to="/signup"
             >
-              Rick & Morty API
+              Sign Up
             </NavLink>
           </li>
         </ul>
       </div>
-      <button className="md:hidden" onClick={toggleMenu}>
+      <button className="md:hidden z-10" onClick={toggleMenu}>
         {isOpen ? <X color="#9FC315" /> : <Menu color="#9FC315" />}
       </button>
     </div>
