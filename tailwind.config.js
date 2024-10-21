26535f8040ebe44631354130af60ada63b55ca05
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-10-21 12:44:40 +0300

    Initialized a react app using vite and added tailwind css configurations

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
