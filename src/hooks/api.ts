2274e16e63a71793134da7c7b5e28b7eaaba1e86
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-10-29 10:40:24 +0300

    fixed backend url in the api hook

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const backend_url = import.meta.env.VITE_BACKEND_URL as string;
+const backend_url = 'https://savannah-task-backend.onrender.com/api'
 const token = localStorage.getItem('authToken');
 
 const api = axios.create({
